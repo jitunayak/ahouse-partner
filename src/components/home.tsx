666398276a5d9c849c262d9bc4a486690293ca8f
jitunayak <jitunayak715@gmail.com>
2024-11-25 01:33:14 +0530

    Remove unused UserManagement import from home.tsx

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -1,7 +1,6 @@
 import { Outlet } from "@tanstack/react-router";
 import { AppSidebar } from "./ui/app-sidebar";
 import { SidebarProvider, SidebarTrigger } from "./ui/sidebar";
-import { UserManagement } from "./user-management";
 
 export const Home = () => {
   return (
