c30142ddb7b1c86baf032803ef08087149f536eb
jitunayak <jitunayak715@gmail.com>
2024-11-26 11:57:17 +0530

    Add auction fetching logic and improve Inbox component layout and error handling

--- src/components/inbox.tsx
+++ src/components/inbox.tsx
@@ -1,38 +1,75 @@
+import { useAuthStore } from "@/hooks";
+import { supabase } from "@/supabaseClient";
+import { useQuery } from "@tanstack/react-query";
 import { CheckCheckIcon, Terminal, Trash2Icon } from "lucide-react";
-import { Alert, AlertTitle, AlertDescription } from "./ui/alert";
+import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
 import { Button } from "./ui/button";
 
 export const Inbox = () => {
+  const auth = useAuthStore();
+  const getUnApprovedAuctions = async () => {
+    const { data, error } = await supabase
+      .from("auctions")
+      .select("id, title, description, status, images, case_number")
+      .eq("org_id", auth.user?.org_id);
+    if (error) {
+      throw new Error(error.message);
+    }
+    return data;
+  };
+
+  const { data, isPending, error, isSuccess, isError } = useQuery({
+    queryKey: ["auctions", auth.user?.org_id],
+    queryFn: getUnApprovedAuctions,
+  });
+
+  if (isPending || !isSuccess) return <p>Loading...</p>;
+
+  if (isError) {
+    return <p>Error: {error}</p>;
+  }
   return (
     <div className="space-y-4 gap-4 p-6 justify-center flex flex-col w-screen">
       <h1 className="text-2xl font-bold">Inbox</h1>
 
-      <div className="w-full lg:w-[calc(100%-16rem)]">
-        <Alert className="flex justify-between">
-          <div>
-            <Terminal className="h-4 w-4 my-2" />
-            <AlertTitle>Auction Name</AlertTitle>
-            <AlertDescription>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-            </AlertDescription>
-            <div className="text-xs text-gray-500 py-2">
-              Submitted by: John Doe
+      {data.length === 0 && <p>No Unapproved Auctions</p>}
+      <div className="w-full lg:w-[calc(100%-16rem)] space-y-4">
+        {data.map((auction) => {
+          return (
+            <div key={auction.id}>
+              <Alert className="flex justify-between">
+                <div className="flex">
+                  <img
+                    src={auction.images[0]}
+                    alt="image"
+                    className="rounded w-36 h-24 aspect-auto mr-2 object-fill"
+                  />
+                  <div>
+                    <Terminal className="h-4 w-4 my-2" />
+                    <AlertTitle>{auction.title}</AlertTitle>
+                    <AlertDescription>{auction.description}</AlertDescription>
+                    <div className="text-xs text-gray-500 py-2">
+                      Submitted by: John Doe
+                    </div>
+                  </div>
+                </div>
+                <div className="flex gap-2 self-end">
+                  <Button variant={"outline"} size={"sm"}>
+                    View
+                  </Button>
+                  <Button variant="destructive" size={"sm"}>
+                    <Trash2Icon className="h-4 w-4" />
+                    Delete
+                  </Button>
+                  <Button variant="default" size={"sm"}>
+                    <CheckCheckIcon className="h-4 w-4" />
+                    Approve
+                  </Button>
+                </div>
+              </Alert>
             </div>
-          </div>
-          <div className="flex gap-2 self-end">
-            <Button variant={"outline"} size={"sm"}>
-              View
-            </Button>
-            <Button variant="destructive" size={"sm"}>
-              <Trash2Icon className="h-4 w-4" />
-              Delete
-            </Button>
-            <Button variant="default" size={"sm"}>
-              <CheckCheckIcon className="h-4 w-4" />
-              Approve
-            </Button>
-          </div>
-        </Alert>
+          );
+        })}
       </div>
     </div>
   );
--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,8 +1,8 @@
-import * as React from "react";
-import { Outlet, createRootRoute } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { Toaster } from "@/components/ui/sonner";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
+import * as React from "react";
 
 export const Route = createRootRoute({
   component: RootComponent,
@@ -13,6 +13,8 @@ function RootComponent() {
     defaultOptions: {
       queries: {
         staleTime: Infinity,
+        refetchOnReconnect: true,
+        refetchOnWindowFocus: true,
       },
     },
   });
