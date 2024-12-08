2dad456fa477644d2e39fdf1a0e6a719647dc67c
jitunayak <jitunayak715@gmail.com>
2024-12-08 18:23:25 +0530

    added react query dev tools

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -3,11 +3,11 @@ import { useStore } from "@/hooks";
 import { queryClient } from "@/lib";
 import { supabase } from "@/supabaseClient";
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Outlet, createRootRoute, useRouter } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import * as React from "react";
 import { useShallow } from "zustand/react/shallow";
-
 export const Route = createRootRoute({
   component: RootComponent,
 });
@@ -33,6 +33,7 @@ function RootComponent() {
         <Outlet />
         <Toaster richColors theme="light" expand closeButton />
         <TanStackRouterDevtools position="bottom-right" />
+        <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </React.Fragment>
   );
