a903e39784e6d04364c98cc8ca31f70d9280f8ce
jitunayak <jitunayak715@gmail.com>
2025-01-10 13:28:07 +0530

    Fixed date validation logic in date picker

--- src/components/update-auction.tsx
+++ src/components/update-auction.tsx
@@ -125,7 +125,7 @@ function UpdateAuction() {
                               selected={field.value}
                               onSelect={field.onChange}
                               disabled={(date: Date) =>
-                                date > new Date() ||
+                                date < new Date() ||
                                 date < new Date("1900-01-01")
                               }
                               initialFocus
