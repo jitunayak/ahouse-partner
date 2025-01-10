fd24742a816e9930146973c6fdee196d094950b8
jitunayak <jitunayak715@gmail.com>
2025-01-10 20:50:07 +0530

    Added useRouter and auth navigation logic

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,5 @@
 import { supabase } from "@/supabaseClient";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
 import * as React from "react";
 
 export const Route = createFileRoute("/")({
@@ -7,15 +7,36 @@ export const Route = createFileRoute("/")({
 });
 
 function RouteComponent() {
-  // const router = useRouter();
+  const router = useRouter();
   React.useEffect(() => {
-    // supabase.auth.getSession().then(({ data: { session } }) => {
-    //   if (session) {
-    //     router.navigate({ to: "/home", replace: true });
-    //   } else {
-    //     router.navigate({ to: "/login", replace: true });
-    //   }
-    // });
-  }, [supabase.auth.onAuthStateChange]);
+    const { data } = supabase.auth.onAuthStateChange((event, session) => {
+      console.log(event, session);
+      if (event === "SIGNED_IN") {
+        router.navigate({ to: "/home", replace: true });
+        // handle sign in event
+      } else if (event === "SIGNED_OUT") {
+        // handle sign out event
+        router.navigate({ to: "/login", replace: true });
+      } else if (event === "PASSWORD_RECOVERY") {
+        // handle password recovery event
+        router.navigate({ to: "/update-password", replace: true });
+      } else if (event === "TOKEN_REFRESHED") {
+        // handle token refreshed event
+      } else if (event === "USER_UPDATED") {
+        // handle user updated event
+      }
+      if (event === "INITIAL_SESSION") {
+        // handle initial session
+        if (!session) {
+          router.navigate({ to: "/login", replace: true });
+        }
+      }
+    });
+
+    return () => {
+      // call unsubscribe to remove the callback
+      data.subscription.unsubscribe();
+    };
+  }, []);
   return "";
 }
